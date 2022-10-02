671f7ab6d3e18a229f0a32f2e58c699865000a89
ManuCiao10 <emanuele.ardinghi@gmail.com>
2022-10-01 20:28:13 -0500

    eagle 0.0.1

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/Manuciao10/bot
+
+go 1.19
+
+require (
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.1 // indirect
+	github.com/xdg-go/stringprep v1.0.3 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.10.2 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	// "reflect"
+	"strings"
+	"time"
+
+	"github.com/fatih/color"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+
+	err := godotenv.Load("config/.env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func SelectMode(label string) string {
+	var s string
+	r := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Fprint(os.Stderr, label+" ")
+		s, _ = r.ReadString('\n')
+		if s != "" {
+			break
+		}
+	}
+	return strings.TrimSpace(s)
+}
+
+func Read_json() bool {
+	var key string
+	content, err := os.ReadFile("./setting.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var payload map[string]interface{}
+	err = json.Unmarshal(content, &payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+	key = payload["key"].(string)
+	uuid := payload["uuid"].(string)
+	return Read_database(key, uuid) // HE'S is gonna return true or false
+}
+
+func Read_database(key string, uuid string) bool {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://" + os.Getenv("USERNAME") + ":" + os.Getenv("PASSWORD") + "@cluster0.8azzuqv.mongodb.net/?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	col := client.Database("autentichation").Collection("autentichation")
+	cursor, err := col.Find(context.TODO(), bson.D{})
+	if err != nil {
+		fmt.Println("Finding all documents ERROR:", err)
+		defer cursor.Close(ctx)
+	} else {
+		// iterate over docs using Next()
+		for cursor.Next(ctx) {
+			// Declare a result BSON object
+			var result bson.M
+			err := cursor.Decode(&result)
+			if err != nil {
+				fmt.Println("cursor.Next() error:", err)
+				os.Exit(1)
+				// If there are no cursor.Decode errors
+			} else {
+				if result[uuid] == key {
+					// fmt.Println("\nresult type:", reflect.TypeOf(result))
+					// fmt.Println("result:", result)
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
+func main() {
+	color.Red("CHECKING KEY...")
+	if !Read_json() {
+		color.Red("KEY NOT VALID")
+		os.Exit(1)
+	}
+	content, err := os.ReadFile("logo.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	color.Red(string(content))
+
+	color.Red("[ Eagle 0.0.2 ]" + "[ " + time.Now().Format("15:04:05.000000") + " ]" + " 1. GAMESTOP")
+	color.Red("[ Eagle 0.0.2 ]" + "[ " + time.Now().Format("15:04:05.000000") + " ]" + " 2. UNIEURO")
+	color.Red("[ Eagle 0.0.2 ]" + "[ " + time.Now().Format("15:04:05.000000") + " ]" + " 3. DADSTOCK")
+	println("\n")
+
+	mode := SelectMode("[ Eagle 0.0.2 ]" + "[ " + time.Now().Format("15:04:05.000000") + " ]" + " PLEASE SELECT SITE:")
+	if mode == "1" {
+		print("GAMESTOP")
+	} else if mode == "2" {
+		print("UNIEURO")
+	} else if mode == "3" {
+		print("DADSTOCK")
+	} else {
+		print("INVALID CHOICE!\n") //ADD RESTART BOT
+	}
+	// fmt.Println(mode)
+}
+
+// check if the key an dthe uuid is valid (SQL)
+// check the ID of the Machine
+// Add Dashboard
+// Add monitor
+// Add client
+// Add sql database
+
+//------------------------------------------------------------------//
+// ADD a guide to get the uuid
