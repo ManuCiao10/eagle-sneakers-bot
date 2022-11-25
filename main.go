b0bd43d77ebd72142b614c34d1efbdfc553ef367
ManuCiao10 <emanuele.ardinghi@gmail.com>
2022-11-25 08:12:33 -0500

    Sitelist profile

--- main.go
+++ main.go
@@ -15,10 +15,10 @@ import (
 	"github.com/eagle/utils"
 	"github.com/fatih/color"
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	// "go.mongodb.org/mongo-driver/bson"
+	// "go.mongodb.org/mongo-driver/bson/primitive"
+	// "go.mongodb.org/mongo-driver/mongo"
+	// "go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func init() {
@@ -30,126 +30,126 @@ func init() {
 	}
 }
 
-func Change_id(ID_object string) {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://" + os.Getenv("USERNAME") + ":" + os.Getenv("PASSWORD") + "@cluster0.8azzuqv.mongodb.net/?retryWrites=true&w=majority"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	err = client.Connect(ctx)
+// func Change_id(ID_object string) {
+// 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://" + os.Getenv("USERNAME") + ":" + os.Getenv("PASSWORD") + "@cluster0.8azzuqv.mongodb.net/?retryWrites=true&w=majority"))
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+// 	defer cancel()
+// 	err = client.Connect(ctx)
 
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Disconnect(ctx)
-	col := client.Database("autentichation").Collection("autentichation")
-	id_obj, _ := primitive.ObjectIDFromHex(ID_object)
-	res, err := col.UpdateOne(
-		ctx,
-		bson.M{"_id": id_obj},
-		bson.D{
-			{Key: "$set", Value: bson.D{{Key: "id", Value: utils.Gen_id()}}},
-		},
-	)
-	_ = res
-	if err != nil {
-		color.HiMagenta("[ " + time.Now().Format("15:04:05.000000") + " ]" + "OPEN TICKET ERROR TO UPDATE KEY")
-		os.Exit(1)
-	}
-	color.HiMagenta("[ " + time.Now().Format("15:04:05.000000") + " ]" + " ID UPDATED RESTART BOT")
-	os.Exit(1)
-}
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer client.Disconnect(ctx)
+// 	col := client.Database("autentichation").Collection("autentichation")
+// 	id_obj, _ := primitive.ObjectIDFromHex(ID_object)
+// 	res, err := col.UpdateOne(
+// 		ctx,
+// 		bson.M{"_id": id_obj},
+// 		bson.D{
+// 			{Key: "$set", Value: bson.D{{Key: "id", Value: utils.Gen_id()}}},
+// 		},
+// 	)
+// 	_ = res
+// 	if err != nil {
+// 		color.HiMagenta("[ " + time.Now().Format("15:04:05.000000") + " ]" + "OPEN TICKET ERROR TO UPDATE KEY")
+// 		os.Exit(1)
+// 	}
+// 	color.HiMagenta("[ " + time.Now().Format("15:04:05.000000") + " ]" + " ID UPDATED RESTART BOT")
+// 	os.Exit(1)
+// }
 
-func CheckId(id_database string, ID_object string) {
-	if id_database == "" {
-		Change_id(ID_object)
-	}
-	if utils.Gen_id() == id_database {
-		color.HiMagenta("[" + time.Now().Format("15:04:05.000000") + "]" + " ID VALID")
-	} else {
-		color.HiMagenta("[ " + time.Now().Format("15:04:05.000000") + " ]" + " ID NOT VALID")
-		answer := utils.SelectMode("[ " + time.Now().Format("15:04:05.000000") + " ]" + " DO YOU WANT TO RESET IT? (Y/N): ")
-		if answer == "y" || answer == "Y" || answer == "yes" || answer == "YES" {
-			Change_id(ID_object)
-			color.HiMagenta("[ " + time.Now().Format("15:04:05.000000") + " ]" + " ID UPDATED")
-			os.Exit(1)
-		} else {
-			color.HiMagenta("[ " + time.Now().Format("15:04:05.000000") + " ]" + " ID NOT UPDATED")
-			os.Exit(1)
-		}
-	}
+// func CheckId(id_database string, ID_object string) {
+// 	if id_database == "" {
+// 		Change_id(ID_object)
+// 	}
+// 	if utils.Gen_id() == id_database {
+// 		color.HiMagenta("[" + time.Now().Format("15:04:05.000000") + "]" + " ID VALID")
+// 	} else {
+// 		color.HiMagenta("[ " + time.Now().Format("15:04:05.000000") + " ]" + " ID NOT VALID")
+// 		answer := utils.SelectMode("[ " + time.Now().Format("15:04:05.000000") + " ]" + " DO YOU WANT TO RESET IT? (Y/N): ")
+// 		if answer == "y" || answer == "Y" || answer == "yes" || answer == "YES" {
+// 			Change_id(ID_object)
+// 			color.HiMagenta("[ " + time.Now().Format("15:04:05.000000") + " ]" + " ID UPDATED")
+// 			os.Exit(1)
+// 		} else {
+// 			color.HiMagenta("[ " + time.Now().Format("15:04:05.000000") + " ]" + " ID NOT UPDATED")
+// 			os.Exit(1)
+// 		}
+// 	}
 
-}
+// }
 
-func Read_json() bool {
-	color.Red("[" + time.Now().Format("15:04:05.000000") + "]" + " CHECKING KEY...")
+// func Read_json() bool {
+// 	color.Red("[" + time.Now().Format("15:04:05.000000") + "]" + " CHECKING KEY...")
 
-	var key string
-	content, err := os.ReadFile("eaglebot/bin/release/setting.json")
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	var key string
+// 	content, err := os.ReadFile("eaglebot/bin/release/setting.json")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	var payload map[string]interface{}
-	err = json.Unmarshal(content, &payload)
-	if err != nil {
-		log.Fatal(err)
-	}
-	key = payload["key"].(string)
-	uuid := payload["uuid"].(string)
-	return Read_database(key, uuid) // HE'S is gonna return true or false
-}
+// 	var payload map[string]interface{}
+// 	err = json.Unmarshal(content, &payload)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	key = payload["key"].(string)
+// 	uuid := payload["uuid"].(string)
+// 	return Read_database(key, uuid) // HE'S is gonna return true or false
+// }
 
-func Read_database(key string, uuid string) bool {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://" + os.Getenv("USERNAME") + ":" + os.Getenv("PASSWORD") + "@cluster0.8azzuqv.mongodb.net/?retryWrites=true&w=majority"))
-	if err != nil {
-		deadstock.Print_err("CONNECTION ERROR D")
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	err = client.Connect(ctx)
+// func Read_database(key string, uuid string) bool {
+// 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://" + os.Getenv("USERNAME") + ":" + os.Getenv("PASSWORD") + "@cluster0.8azzuqv.mongodb.net/?retryWrites=true&w=majority"))
+// 	if err != nil {
+// 		deadstock.Print_err("CONNECTION ERROR D")
+// 	}
+// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+// 	defer cancel()
+// 	err = client.Connect(ctx)
 
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Disconnect(ctx)
-	col := client.Database("autentichation").Collection("autentichation")
-	cursor, err := col.Find(context.TODO(), bson.D{})
-	if err != nil {
-		fmt.Println("Finding all documents ERROR:", err)
-		defer cursor.Close(ctx)
-	} else {
-		// iterate over docs using Next()
-		for cursor.Next(ctx) {
-			// Declare a result BSON object
-			var result bson.M
-			err := cursor.Decode(&result)
-			if err != nil {
-				fmt.Println("cursor.Next() error:", err)
-				os.Exit(1)
-				// If there are no cursor.Decode errors
-			} else {
-				if result[uuid] == key {
-					// fmt.Println("\nresult type:", reflect.TypeOf(result))
-					ID_object := result["_id"].(primitive.ObjectID).Hex()
-					CheckId(result["id"].(string), ID_object)
-					return true
-				}
-			}
-		}
-	}
-	return false
-}
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer client.Disconnect(ctx)
+// 	col := client.Database("autentichation").Collection("autentichation")
+// 	cursor, err := col.Find(context.TODO(), bson.D{})
+// 	if err != nil {
+// 		fmt.Println("Finding all documents ERROR:", err)
+// 		defer cursor.Close(ctx)
+// 	} else {
+// 		// iterate over docs using Next()
+// 		for cursor.Next(ctx) {
+// 			// Declare a result BSON object
+// 			var result bson.M
+// 			err := cursor.Decode(&result)
+// 			if err != nil {
+// 				fmt.Println("cursor.Next() error:", err)
+// 				os.Exit(1)
+// 				// If there are no cursor.Decode errors
+// 			} else {
+// 				if result[uuid] == key {
+// 					// fmt.Println("\nresult type:", reflect.TypeOf(result))
+// 					ID_object := result["_id"].(primitive.ObjectID).Hex()
+// 					CheckId(result["id"].(string), ID_object)
+// 					return true
+// 				}
+// 			}
+// 		}
+// 	}
+// 	return false
+// }
 
 func main() {
-	// key.Initialize()
+	// loading.Initialize()
 	auth.Initialize()
 
-	if !Read_json() {
-		color.Red("KEY NOT VALID")
-		os.Exit(255)
-	}
+	// if !Read_json() {
+	// 	color.Red("KEY NOT VALID")
+	// 	os.Exit(255)
+	// }
 	rich_presence.Initialize()
 	// loading.Initialize()
 	// console.Initialize()
