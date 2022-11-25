dab6f63fde543da5be258e99319a3206204a5fb5
ManuCiao10 <emanuele.ardinghi@gmail.com>
2022-11-24 23:14:09 -0500

    Deploy PORT

--- eaglebot/handler/auth/auth.go
+++ eaglebot/handler/auth/auth.go
@@ -43,6 +43,6 @@ func Initialize() {
 		io.WriteString(w, "Hello, world!\n")
 	}
 	http.HandleFunc("/", helloHandler)
-	log.Println("Listing for" + port)
+	log.Println("Listing for " + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
