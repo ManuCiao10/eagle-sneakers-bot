b9df1e18ec56169792087c68df6cfad981ee25e3
admin <emanuele.ardinghi@gmail.com>
2023-03-28 01:07:16 +0200

    fixed main

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"github.com/eagle/handler/auth"
 	"github.com/eagle/handler/cmd"
+	"github.com/eagle/handler/cmd/console"
 	"github.com/eagle/handler/cmd/console_mac"
 	"github.com/eagle/handler/create"
 	"github.com/eagle/handler/loading"
 	"github.com/eagle/handler/logs"
+	"github.com/eagle/handler/version"
+	"github.com/eagle/handler/ws_quicktasking"
 
 	"github.com/eagle/handler/modules/fiver"
 	"github.com/eagle/handler/modules/nike"
@@ -18,22 +21,12 @@ import (
 	"github.com/eagle/handler/utils"
 )
 
-//go:generate goversioninfo -skip-versioninfo=true -icon=handler/create/favicon.ico 
+//go:generate goversioninfo -skip-versioninfo=true -icon=handler/create/favicon.ico
 
 func main() {
-	// w := webview.New(true)
-	// defer w.Destroy()
-	// w.SetTitle("Eagle")
-	// w.SetSize(800, 600, webview.HintNone)
-	// w.Bind("quit", func() {
-	// 	w.Terminate()
-	// })
-
-	// w.Run()
-
 	create.Initialize()
 	loading.Initialize()
-	// console.Initialize() //only windows
+	console.Initialize() //only windows
 	console_mac.Initialize()
 	quicktasking.Initialize()
 	nike.Initialize()
@@ -42,9 +35,9 @@ func main() {
 	fiver.Initialize()
 	logs.LogtailInitialize()
 
-	// ws_quicktasking.Initialize() //to be implemented
-	// auth.Initialize() //to be fixed
-	// version.Initialize() //to be fixed
+	ws_quicktasking.Initialize() //to be implemented
+	auth.Initialize()            //to be fixed
+	version.Initialize()         //to be fixed
 
 	presence.Initialize()
 	utils.Banner()
